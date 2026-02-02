c0202bd8cc5a54786d6c7e203dc93f33634d7aa5
alvin-ictn <55023127+alvin-ictn@users.noreply.github.com>
2026-02-02 08:06:15 +0700

    feat: Add PostgreSQL database initialization logic with pgx driver.

--- database/database.go
+++ database/database.go
@@ -3,13 +3,46 @@ package database
 import (
 	"database/sql"
 	"log"
+	"net"
+	"net/url"
 
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func InitDB(connectionString string) (*sql.DB, error) {
+	// Attempt to resolve hostname to IPv4 to avoid IPv6 issues
+	resolvedConnString := connectionString
+	u, err := url.Parse(connectionString)
+	if err == nil && u.Host != "" {
+		host := u.Hostname()
+		port := u.Port()
+
+		// Only resolve if it's not already an IP
+		if net.ParseIP(host) == nil {
+			ips, err := net.LookupIP(host)
+			if err == nil {
+				var ipv4 net.IP
+				for _, ip := range ips {
+					if ip.To4() != nil {
+						ipv4 = ip
+						break
+					}
+				}
+				if ipv4 != nil {
+					log.Printf("Resolved database host %s to %s", host, ipv4.String())
+					if port != "" {
+						u.Host = net.JoinHostPort(ipv4.String(), port)
+					} else {
+						u.Host = ipv4.String()
+					}
+					resolvedConnString = u.String()
+				}
+			}
+		}
+	}
+
 	// Open database
-	db, err := sql.Open("postgres", connectionString)
+	db, err := sql.Open("pgx", resolvedConnString+"&prefer_simple_protocol=true")
 	if err != nil {
 		return nil, err
 	}
--- go.mod
+++ go.mod
@@ -23,6 +23,10 @@ require (
 	github.com/go-openapi/swag/typeutils v0.25.4 // indirect
 	github.com/go-openapi/swag/yamlutils v0.25.4 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.9.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
